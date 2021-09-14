7623c5feb65af5c9ab69b9d681b7c24d1daf7cfc
Grant Sparks <grantpatricksparks@gmail.com>
2021-09-13 23:55:04 -0500

    semicolon formatting (cpu6502.js)

--- src/nellie/cpu6502.js
+++ src/nellie/cpu6502.js
@@ -6,7 +6,7 @@ const Ellie = require('@ellieproject/ellie');
 
 const CPU6502 = new Ellie.Processor('6502');
 
-CPU6502.OPCODES = require('./cpu6502/opcodes.js')
+CPU6502.OPCODES = require('./cpu6502/opcodes.js');
 
 for (const name in CPU6502.OPCODES) {
   CPU6502.addOpcode(CPU6502.OPCODES[name]);
